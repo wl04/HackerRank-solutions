8572e1beb30fbc881a85d9a0768de95be394e4bb
wl04 <30796494+wl04@users.noreply.github.com>
2021-05-03 13:32:44 +0300

    Create full solution.java

---
+++ Java/Data Structures/Java Stack/full solution.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Solution{
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		Stack<Character> stk = new Stack<>();
+        
+		while (sc.hasNext()) {
+			String input=sc.next();
+            if (input.isEmpty()) {
+                System.out.println("true"); // Always true if we have empty string   
+            }
+            else {
+                for (int i = 0; i < input.length(); i++) {
+                    if (!stk.isEmpty()) { // Checking if stack is empty to prevent error
+                        switch (input.charAt(i)) {
+                            // we pop for closing parantheses
+                            case ')':
+                                if (stk.peek() == '(') {stk.pop();}     
+                                break;
+                            case ']':
+                                if (stk.peek() == '[') {stk.pop();}     
+                                break;
+                            case '}':
+                                if (stk.peek() == '{') {stk.pop();}     
+                                break;   
+                            // we push for opening parantheses
+                            case '(':
+                            case '[':
+                            case '{':
+                                stk.push(input.charAt(i)); 
+                                break; 
+                        }
+                    }
+                    else {
+                        stk.push(input.charAt(i));
+                    } 
+                }
+                // If stack is empty then all parentheses are balanced
+                System.out.println(stk.isEmpty()); 
+                stk.clear(); // dont forget to empty stack    
+            }
+		}
+	}
+}
