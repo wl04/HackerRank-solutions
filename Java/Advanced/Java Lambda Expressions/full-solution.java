95bc50494a017fad0bc027b0d635c3d9cabd3d6f
wl04 <30796494+wl04@users.noreply.github.com>
2021-05-04 23:29:28 +0300

    Create full-solution.java

---
+++ Java/Advanced/Java Lambda Expressions/full-solution.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+interface PerformOperation {
+ boolean check(int a);
+}
+class MyMath {
+ public static boolean checker(PerformOperation p, int num) {
+  return p.check(num);
+ }
+    // Solution
+    public PerformOperation isOdd() {
+        return (int x) -> x % 2 == 0 ? false : true; 
+    }
+    
+    public PerformOperation isPrime() {
+        return (int x) -> {
+            for (int i = 2; i < x / 2; i++) {
+                if (x % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+    }
+    
+    public PerformOperation isPalindrome() {
+        return (int x) -> {
+            int r, sum = 0;
+            int temp = x;
+            
+            while (x > 0) {
+                r = x % 10;
+                sum = (sum * 10) + r;
+                x = x / 10;
+            }        
+            return temp == sum ? true : false;
+        };
+    }
+}
+
+public class Solution {
+
+ public static void main(String[] args) throws IOException {
+  MyMath ob = new MyMath();
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  int T = Integer.parseInt(br.readLine());
+  PerformOperation op;
+  boolean ret = false;
+  String ans = null;
+  while (T--> 0) {
+   String s = br.readLine().trim();
+   StringTokenizer st = new StringTokenizer(s);
+   int ch = Integer.parseInt(st.nextToken());
+   int num = Integer.parseInt(st.nextToken());
+   if (ch == 1) {
+    op = ob.isOdd();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "ODD" : "EVEN";
+   } else if (ch == 2) {
+    op = ob.isPrime();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PRIME" : "COMPOSITE";
+   } else if (ch == 3) {
+    op = ob.isPalindrome();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+   }
+   System.out.println(ans);
+  }
+ }
+}
