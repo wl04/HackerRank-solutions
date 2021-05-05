25aec2a22c1d10d9fb53ada29b9ea46de0602367
wl04 <30796494+wl04@users.noreply.github.com>
2021-05-05 18:34:51 +0300

    Create full-solution.java

---
+++ Problem Solving/Data Structures/Arrays/Left Rotation/full-solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        // Initializing new List filled with 0 and size of arr
+        List<Integer> leftRot = Arrays.asList(new Integer[arr.size()]);
+        
+        // Iterating through initial array
+        for (int i = 0; i < arr.size(); i++) {     
+            // (i + arr.size() - d) % arr.size to calculate new position
+            leftRot.set((i + arr.size() - d) % arr.size(), arr.get(i));
+        }
+        return leftRot;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> result = Result.rotateLeft(d, arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
