c02b26981c466b6977006a0d7336615221023746
wl04 <30796494+wl04@users.noreply.github.com>
2021-05-09 18:33:50 +0300

    Create solution.java

---
+++ Problem Solving/Data Structures/Trees/Tree: Preorder Traversal/solution.java
@@ -0,0 +1,9 @@
+    // Solution using recursive calls
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
