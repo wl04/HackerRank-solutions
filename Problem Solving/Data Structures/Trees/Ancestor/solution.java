90331b7173eaded981cde93de8f69a6537db71d5
wl04 <30796494+wl04@users.noreply.github.com>
2021-05-10 16:38:22 +0300

    Create solution.java

---
+++ Problem Solving/Data Structures/Trees/Ancestor/solution.java
@@ -0,0 +1,18 @@
+  // Solution - main idea node data we search is always between v1 and v2
+	public static Node lca(Node root, int v1, int v2) {
+      	if (root == null) {
+            return null;
+        }
+          
+        // If data is smaller than both v1 and v2
+        if (root.data < v1 && root.data < v2) {
+            return lca(root.right, v1, v2);
+        }
+        // If data is bigger than both v1 and v2
+        if (root.data > v1 && root.data > v2) {
+            return lca(root.left, v1, v2);
+        }
+        
+        // Else we found our node
+        return root;
+    }
