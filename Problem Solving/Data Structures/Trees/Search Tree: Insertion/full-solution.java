35cef81298128b945830a7ab84b137b775f90dcb
wl04 <30796494+wl04@users.noreply.github.com>
2021-05-10 15:19:24 +0300

    Create full-solution.java

---
+++ Problem Solving/Data Structures/Trees/Search Tree: Insertion/full-solution.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+   
+  	public static void preOrder( Node root ) {
+      
+    	if( root == null)
+        	return;
+      
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+     
+    }
+    
+  // Solution
+	public static Node insert(Node root,int data) {
+        // If the tree is empty return a new node
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        // Otherwise, recur down the tree 
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }	
+        // Return unchanged node pointer
+        return root;
+    }
+
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+      	preOrder(root);
+    }	
+}
