0332bed6b421c3a150fae63e188e0b611c243e94
wl04 <30796494+wl04@users.noreply.github.com>
2021-05-12 08:54:23 +0300

    Create full-solution.java

---
+++ Interview Preparation Kit/Arrays/Arrays: Left Rotation/full-solution.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    
+    // Simple O(n) time and O(n) space complexity solution
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+        // Init new array of same size
+        int size = a.size();
+        List<Integer> newA = Arrays.asList(new Integer[size]);
+        
+        // Using modular to calculate new position
+        for(int i = 0; i < size; i++) {
+            int newPos = (i + (size - d)) % size;
+            newA.set(newPos, a.get(i));
+        }
+        return newA;   
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.rotLeft(a, d);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
